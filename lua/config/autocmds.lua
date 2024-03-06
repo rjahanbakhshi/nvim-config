1ecf737b5806533d362a00234ef48ddb2fee4ca7
Reza Jahanbakhshi <reza.jahanbakhshi@gmail.com>
2024-03-06 09:49:29 +0100

    Workaround for autocmd tab key function params

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,26 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Workaround for autocmd tab key function params 
+--local luasnip = require('luasnip')
+--
+--vim.api.nvim_create_autocmd("ModeChanged", {
+--  group = vim.api.nvim_create_augroup("UnlinkLuaSnipSnippetOnModeChange", {
+--    clear = true,
+--  }),
+--  pattern = { "s:n", "i:*" },
+--  desc = "Forget the current snippet when leaving the insert mode",
+--  callback = function(evt)
+--    -- If we have n active nodes, n - 1 will still remain after a `unlink_current()` call.
+--    -- We unlink all of them by wrapping the calls in a loop.
+--    while true do
+--      if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
+--        luasnip.unlink_current()
+--      else
+--        break
+--      end
+--    end
+--  end,
+--})
+
