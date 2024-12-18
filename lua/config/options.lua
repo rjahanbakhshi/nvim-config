6ecf1338217ce83d7f34be286f7adb198dac8cfb
Reza Jahanbakhshi <reza.jahanbakhshi@gmail.com>
2024-12-18 07:51:52 +0100

    Added some more langs and plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,5 @@ vim.g.autoformat = false
 vim.opt.relativenumber = false
 vim.opt.colorcolumn = "80"
 vim.opt.spell = true
+-- Use cwd as root. Default is vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 vim.g.root_spec = { "cwd" }
