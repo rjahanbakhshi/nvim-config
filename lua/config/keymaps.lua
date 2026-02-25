423b40d92120f089e5cd8672a462256de75f1fe5
Reza Jahanbakhshi <reza.jahanbakhshi@gmail.com>
2026-02-25 07:16:33 +0100

    Sync

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,20 @@ vim.keymap.set({ "n", "i", "t" }, "<C-S-Up>", "<C-\\><C-n><C-w>k", { desc = "Mov
 vim.keymap.set({ "n", "i", "t" }, "<C-S-Down>", "<C-\\><C-n><C-w>j", { desc = "Move to window below" })
 vim.keymap.set({ "n", "i", "t" }, "<C-S-Left>", "<C-\\><C-n><C-w>h", { desc = "Move to left window" })
 vim.keymap.set({ "n", "i", "t" }, "<C-S-Right>", "<C-\\><C-n><C-w>l", { desc = "Move to right window" })
+
+-- Delete without overwriting clipboard
+vim.keymap.set({ "n", "v" }, "d", '"_d', { desc = "Delete without yanking" })
+vim.keymap.set({ "n", "v" }, "D", '"_D', { desc = "Delete to end without yanking" })
+vim.keymap.set({ "n", "v" }, "<Del>", '"_x', { desc = "Delete char without yanking" })
+vim.keymap.set("n", "d<Up>", function()
+  local line = vim.fn.line(".")
+  if line > 1 then
+    vim.api.nvim_buf_set_lines(0, line - 2, line - 1, false, {})
+  end
+end, { desc = "Delete line above without yanking" })
+vim.keymap.set("n", "d<Down>", function()
+  local line = vim.fn.line(".")
+  if line < vim.fn.line("$") then
+    vim.api.nvim_buf_set_lines(0, line, line + 1, false, {})
+  end
+end, { desc = "Delete line below without yanking" })
--- lua/plugins/yanky.lua
+++ lua/plugins/yanky.lua
@@ -6,5 +6,10 @@ return {
         clipboard_register = "clipboard",
       },
     },
+    config = function(_, opts)
+      require("yanky").setup(opts)
+      -- Paste over selection without overwriting clipboard
+      vim.keymap.set("v", "p", '"_dP', { desc = "Paste without yanking selection" })
+    end,
   },
 }
